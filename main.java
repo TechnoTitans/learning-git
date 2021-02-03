25c5671e467d76a854176915aa8ff612a22cda41
Berkan Ottlik <berkott3002@gmail.com>
2021-02-03 15:48:36 -0500

    fancy commit

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hey we are learning git");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Now things are getting fancy...");
+        }
     }
 }
